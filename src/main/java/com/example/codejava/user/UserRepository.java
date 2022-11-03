4ff98a892c41b8c6d68bcc7e531c0e20023affe8
Gean S <geangv@outlook.com>
2022-11-02 23:52:14 -0300

    fiz um teste unitario

---
+++ src/main/java/com/example/codejava/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.codejava.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
---
+++ src/test/java/com/example/codejava/UserRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.codejava;
+
+import com.example.codejava.user.User;
+import com.example.codejava.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew()
+    {
+        User user = new User();
+
+        user.setEmail("p0dwan@outlook.com");
+        user.setPassword("1234567");
+        user.setFirstName("Gean");
+        user.setLastName("da Silva");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
